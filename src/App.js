850c4d82dc549bc4b698da17b02a77479329b230
Bonfanti Cecilia <bonfanticeciliaemilce@gmail.com>
2023-09-27 00:02:03 -0300

    se actualiza contador agregando funcionalidad con imagenes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import './App.css';
-import React, {useState} from "react";
+import React, { useState } from 'react';
+import './App.css';
+import gato from './img/gato.png'; 
+import perro from './img/perro.png';
 
 function App() {
-
   const [contador, setContador] = useState(0);
+  const [animales, setAnimales] = useState([]);
+
+  const agregarAnimal = () => {
+    if (contador >= 0) {
+      setAnimales([...animales, {top: Math.random() * (window.innerHeight - 128), left: Math.random() * (window.innerWidth - 128)}]);
+    } else if (contador < 0 && animales.length > 0) {
+      setAnimales(animales.slice(0, -1));
+    }
+    setContador(contador + 1);
+  }
+
+  
+  const quitarAnimal = () => {
+    if (contador > 0 && animales.length > 0) {
+      setAnimales(animales.slice(0, -1));
+    } else if (contador <= 0) {
+      setAnimales([...animales, {top: Math.random() * window.innerHeight, left: Math.random() * window.innerWidth}]);
+    }
+    setContador(contador - 1);
+  }
 
   return (
     <div className="App">
       <div className='contador'>
-
-        <button className="btn" onClick={()=>setContador(contador-1)}>-</button>
-
+        <button className="btn" onClick={quitarAnimal}>-</button>
         <span className="num">{contador}</span>
-
-        <button className="btn" onClick={()=>{setContador(contador+1)}}>+</button>
-
+        <button className="btn" onClick={agregarAnimal}>+</button>
       </div>
+      {animales.map((animal, index) => (
+        <img key={index} src={contador > 0 ? gato : perro} style={{position: 'absolute', top: animal.top, left: animal.left}} alt="animal" />
+      ))}
     </div>
   );
 }
